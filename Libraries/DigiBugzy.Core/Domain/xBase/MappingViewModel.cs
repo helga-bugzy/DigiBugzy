674826f1fa7abe370b099271f0d054c5886c0752
MAREL\helga.schuring <helga.schuring@marel.com>
2022-07-24 13:16:24 +0200

    base for mapping screens view model

---
+++ Libraries/DigiBugzy.Core/Domain/xBase/MappingViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DigiBugzy.Core.Domain.xBase
+{
+    public class MappingViewModel: BaseEntity
+    {
+        public int WorkingEntityId { get; set; }
+
+        public int MapToEntityId { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsMapped { get; set; }
+    }
+}
