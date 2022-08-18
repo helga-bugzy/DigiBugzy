e15672ab8b01cc1a690ebe10aabcc19d1fa529b1
MAREL\helga.schuring <helga.schuring@marel.com>
2022-08-18 17:21:54 +0200

    fixed table and schema mapping on projectdocuments

--- Libraries/DigiBugzy.Core/Domain/Projects/ProjectDocument.cs
+++ Libraries/DigiBugzy.Core/Domain/Projects/ProjectDocument.cs
@@ -3,6 +3,7 @@ using DigiBugzy.Core.Domain.Administration.Documents;
 
 namespace DigiBugzy.Core.Domain.Projects
 {
+    [Table(name: nameof(ProjectDocument), Schema = DatabaseConstants.Schemas.Project)]
     public class ProjectDocument : BaseProjectSublementEntity
     {
         public int DocumentId { get; set; }
